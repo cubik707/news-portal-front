06c23907168d7897a94befc4151b00c280561d85
Vladislava <stdemidovez@gmail.com>
2025-04-19 15:38:15 +0300

    style: remove important

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -4,21 +4,26 @@ import styles from './login-page.module.scss';
 
 
 const LoginPage = () => {
-
   return (
     <div className={'flex justify-center items-center h-full'}>
       <Paper classes={{ root: styles.root }}>
         <Typography classes={{ root: styles.title }} variant="h5">
           Вход в аккаунт
         </Typography>
-        <TextField
-          className={styles.field}
-          label="E-Mail"
-          error
-          helperText="Неверно указана почта"
-          fullWidth
-        />
-        <TextField className={styles.field} label="Пароль" fullWidth />
+
+        <div className={styles.fieldContainer}>
+          <TextField
+            label="Имя пользователя"
+            error
+            helperText="Неверно указано имя пользователя"
+            fullWidth
+          />
+        </div>
+
+        <div className={styles.fieldContainer}>
+          <TextField label="Пароль" fullWidth />
+        </div>
+
         <Button size="large" variant="contained" fullWidth>
           Войти
         </Button>
